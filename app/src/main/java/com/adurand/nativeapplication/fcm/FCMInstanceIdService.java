c93c7f4da95a5b55bc610db201074615ac33b058
Alejandro Durand <alejandro.durand.90@gmail.com>
2017-02-01 15:03:47 -0300

    Added configuration for receive push notification.

---
+++ app/src/main/java/com/adurand/nativeapplication/fcm/FCMInstanceIdService.java
@@ -0,0 +1,10 @@
+package com.adurand.nativeapplication.fcm;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class FCMInstanceIdService extends FirebaseInstanceIdService {
+
+    public FCMInstanceIdService() {
+    }
+
+}
---
+++ app/src/main/java/com/adurand/nativeapplication/fcm/FCMService.java
@@ -0,0 +1,10 @@
+package com.adurand.nativeapplication.fcm;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class FCMService extends FirebaseMessagingService {
+
+    public FCMService() {
+    }
+
+}
